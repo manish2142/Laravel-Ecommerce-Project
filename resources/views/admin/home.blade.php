e144f292d269168fed48034393711e0a03a07619
Manish <manishsaini2142@gmail.com>
2022-04-10 09:32:24 +0530

    First Commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    
+      <!-- partial -->
+     @include('admin.sidebar')
+      @include('admin.navbar')
+
+        <!-- partial -->
+        
+        @include('admin.body')
+          <!-- partial -->
+        @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
+</html>
+
